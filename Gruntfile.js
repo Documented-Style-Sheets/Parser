56dc06bbdf4fbd06504c7ecfcc51b208f76bc2fc
darcyclarke <darcy@darcyclarke.me>
2013-03-11 01:45:33 -0400

    Removed default options in the Gruntfile and added basic styling fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,11 +11,7 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
-		DSS: {
-			options: {
-				location: __dirname + '/example/'
-			}
-		}
+		DSS: {}
 	});
 
 	grunt.loadTasks('tasks');
