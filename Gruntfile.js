dc8291e4ad99ec06959f223b75e9d20865b3b82d
Jeff Jewiss <jeffjewiss@gmail.com>
2013-03-14 18:21:04 -0400

    Adds jshint and basic asset copying tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,19 +10,54 @@
 
 module.exports = function(grunt) {
 
-	grunt.initConfig({
-		DSS: {
-			docs: {
-				options: {
-					template: 'template/'
-				},
-				files: {
-					'docs/': 'example/**/*.{css,scss,sass,less,styl}'
-				}
-			}
-		}
-	});
+    grunt.initConfig({
+        DSS: {
+            docs: {
+                options: {
+                    template: 'template/'
+                },
+                files: {
+                    'docs/': 'example/**/*.{css,scss,sass,less,styl}'
+                }
+            }
+        },
 
-	grunt.loadTasks('tasks');
-	grunt.registerTask('default', ['DSS']);
+        // 
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js',
+                '<%= nodeunit.tests %>'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+
+        // Before generating any files, remove the docs folder
+        clean: {
+            tests: ['docs']
+        },
+
+        // Unit tests.
+        nodeunit: {
+            tests: ['test/*_test.js']
+        },
+
+    });
+
+    grunt.loadTasks('tasks');
+
+    // Plugins that provide necessary tasks
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+    // Whenever the test task is run, cleanup the docs folder and
+    // then test the result.
+    grunt.registerTask('test', ['clean', 'DSS', 'nodeunit']);
+
+    // By default run the DSS task to create docs
+    grunt.registerTask('default', ['DSS']);
+
 };
