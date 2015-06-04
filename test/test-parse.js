efc84cee06a5e778544257b5eb9bc1de314bb2d4
Andreas Haller <andreas.haller@invision.de>
2015-06-04 13:36:53 +0200

    Fix parsing multiline comments. #67
    - Change the test data to use multi line comments
      It seems that the implementation only works with this data
    - Fix parsing (index was one off)
    - Remove leading * from first line in markup blocks
    
    All tests are passing now
    
    - Ignore npm-debug.log

--- test/test-parse.js
+++ test/test-parse.js
@@ -3,13 +3,24 @@ var fs = require('fs');
 var path = require('path');
 
 exports.testParse = function(test){
-  test.expect(2);
+  test.expect(10);
 
   var fileContents = fs.readFileSync(path.join(__dirname, 'data/button.css'), 'utf8');
   dss.parse(fileContents, {}, function(parsed) {
     var block = parsed.blocks[0];
     test.equal(block.name, 'Button');
     test.equal(block.description, 'Your standard form button.');
+
+    test.equal(block.state.length, 2);
+    test.equal(block.state[0].name, ':hover');
+    test.equal(block.state[0].description, 'Highlights when hovering.');
+
+    test.equal(block.state[1].name, '.smaller');
+    test.equal(block.state[1].description, 'A smaller button');
+
+    test.equal(block.markup.length, 1);
+    test.equal(block.markup[0].example.trim(), '<button>This is a button</button>');
+    test.equal(block.markup[0].escaped.trim(), '&lt;button&gt;This is a button&lt;/button&gt;');
     test.done();
   });
 };
